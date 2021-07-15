a5be5fb67177ddbaac173be6cc73dc1e489bfa06
Sasha <zvezdopad659527@gmail.com>
2021-07-15 12:35:21 +0500

    initial commit

---
+++ desktop/src/ru/bzbzz/birthdayadventure/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.bzbzz.birthdayadventure.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.bzbzz.birthdayadventure.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
